0a1e2e66576cf757f5a71fde4c9266c9b50dcdef
Mark Scherer <euromark@web.de>
2016-06-10 00:38:32 +0200

    Test MathEngine

---
+++ tests/TestCase/Engine/MathEngineTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace Captcha\Test\TestCase\Engine;
+
+use Cake\Core\Configure;
+use Cake\TestSuite\TestCase;
+use Captcha\Engine\MathEngine;
+
+/**
+ *
+ */
+class MathEngineTest extends TestCase {
+
+	/**
+	 * @var \Captcha\Engine\MathEngine
+	 */
+	public $Math;
+
+	/**
+	 * setUp method
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		Configure::write('Captcha', [
+				'debug' => false,
+			]
+		);
+		$config = [];
+		$this->Math = new MathEngine($config);
+	}
+
+	/**
+	 * tearDown method
+	 *
+	 * @return void
+	 */
+	public function tearDown() {
+		parent::tearDown();
+
+		unset($this->Math);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testGenerate() {
+		$result = $this->Math->generate();
+		$this->assertSame(['result', 'image'], array_keys($result));
+	}
+
+}
