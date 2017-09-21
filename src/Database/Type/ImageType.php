d179617f6bee853ad9453314c58dd56f4f362827
mscherer <mark.scherer@spryker.com>
2017-09-21 19:38:00 +0200

    Add phpstan support.

--- src/Database/Type/ImageType.php
+++ src/Database/Type/ImageType.php
@@ -7,25 +7,7 @@ use Cake\Database\Type\BinaryType;
 /**
  * Binary type converter.
  *
- * Use to convert binary values including UUIDs between PHP and the database types.
+ * Used to convert binary values between PHP and the database types.
  */
 class ImageType extends BinaryType {
-
-	/**
-	 * Convert binary into resource handles
-	 *
-	 * @param null|string|resource $value The value to convert.
-	 * @param \Cake\Database\Driver $driver The driver instance to convert with.
-	 * @return resource|null
-	 * @throws \Cake\Core\Exception\Exception
-	 */
-	public function toPHP($value, Driver $driver) {
-		// Do not convert UUIDs into a resource
-		if (is_string($value)) {
-			return $value;
-		}
-
-		return parent::toPHP($value, $driver);
-	}
-
 }
