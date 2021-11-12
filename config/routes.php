629b6827e24700e7faa10427f99f16716cd811cc
mscherer <mark.scherer@spryker.com>
2021-11-12 09:40:57 +0100

    Fix up routes file.

--- config/routes.php
+++ config/routes.php
@@ -7,7 +7,7 @@ use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-Router::plugin('Captcha', ['path' => '/captcha'], function (RouteBuilder $routes) {
+$routes->plugin('Captcha', ['path' => '/captcha'], function (RouteBuilder $routes) {
 	$routes->setExtensions(['png', 'jpg']);
 	$routes->fallbacks(DashedRoute::class);
 });
