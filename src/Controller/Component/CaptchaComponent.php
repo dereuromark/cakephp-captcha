05c307a1d0d5a25ef08c09deb9a3adca35fcce81
mscherer <mark.scherer@spryker.com>
2022-07-25 14:52:28 +0200

    Fix CS

--- src/Controller/Component/CaptchaComponent.php
+++ src/Controller/Component/CaptchaComponent.php
@@ -69,7 +69,7 @@ class CaptchaComponent extends Component {
 			return;
 		}
 
-		if (\method_exists($this->getController()->viewBuilder(), 'addHelpers')) {
+		if (method_exists($this->getController()->viewBuilder(), 'addHelpers')) {
 			$this->getController()->viewBuilder()->addHelpers(['Captcha.Captcha']);
 		} else {
 			$this->getController()->viewBuilder()->setHelpers(['Captcha.Captcha']);
