52b587dd3b7ad271e1f28f3c0359c414785c9758
mscherer <dereuromark@web.de>
2023-11-13 10:05:12 +0100

    Fix up test harness

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -84,14 +84,11 @@ Plugin::getCollection()->add(new CaptchaPlugin());
 
 // Ensure default test connection is defined
 if (!getenv('DB_URL')) {
-	putenv('DB_CLASS=Cake\Database\Driver\Sqlite');
-	putenv('DB_URL="sqlite:///:memory:"');
+	putenv('DB_URL=sqlite:///:memory:');
 }
 
 ConnectionManager::setConfig('test', [
-	'className' => 'Cake\Database\Connection',
-	'driver' => getenv('DB_CLASS') ?: null,
-	'dsn' => getenv('DB_URL') ?: null,
+	'url' => getenv('DB_URL') ?: null,
 	'timezone' => 'UTC',
 	'quoteIdentifiers' => true,
 	'cacheMetadata' => true,
